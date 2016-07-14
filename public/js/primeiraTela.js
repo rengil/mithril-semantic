aff10deee0f0e2cee0cd84c7f68776ca5e86bc9e
rengil <renan.lzgil@gmail.com>
2016-07-14 18:54:50 -0300

    primeira tela

--- public/js/primeiraTela.js
+++ public/js/primeiraTela.js
@@ -1,21 +1,36 @@
+var m = require('mithril');
 var primeiraTela = {};
 
-primeiraTela.view = function () {
-  return <div>
-           <form class='ui large form'>
-             <div class='field'>
-               <input name="text" type="text"
-                      placeholder="login"
-                />
-             </div>
-             <div class='field'>
-               <input name="text" type="pass    word"
-                      placeholder="password"
-                />
-             </div>
-           </form>
-         </div>
+primeiraTela.state = {
+  visualizar: function () {
+    m.route('/segundaTela');
+  }
 }
+
+primeiraTela.view = function () {
+  return <div class = 'ui text container no-margin-left  basic segment'>
+           <div class='ui divided items'>
+             <div class='item'>
+               <div class='content'>
+                  <div class='header'>Regiões de São Paulo</div>
+                  <div class='meta'>
+                    <span> Enviada : 13/02/2016 </span>
+                  </div>
+                  <div class='extra'>
+                    <button class='ui right floated green big button'>
+                     Falar com autor do pedido
+                    </button>
+
+                    <button onclick={primeiraTela.state.visualizarclass}
+                            class='ui right floated button big button'>
+                            Ver detalhes
+                    </button>
+                  </div>
+             </div>
+           </div>
+         </div>
+       </div>;
+  }
 
 
 module.exports = primeiraTela;
