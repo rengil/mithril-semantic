49df7163e104a0e8e73cca718893d2c4d575d66d
renan.gil <renan.zgil@gmail.com>
2016-07-14 12:11:19 -0300

    Now we config browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
-var gulp = require('gulp');
-var server = require('gulp-server-livereload');
+const gulp = require('gulp');
+const server = require('gulp-server-livereload');
+const browserify = require('gulp-browserify');
+const runSequence = require('run-sequence');
+
+gulp.task('browserify', function() {
+  return gulp.src('./public/js/welcome.js')
+    .pipe(browserify())
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('copyMainHtml', function() {
+  return gulp.src('./public/html/main.html')
+    .pipe(gulp.dest('dist/'));
+});
 
 gulp.task('webserver', function() {
-  gulp.src('./public')
+  gulp.src('./dist')
     .pipe(server({
       host: '0.0.0.0',
       livereload: true,
-      fallback: '/main.html',
+      fallback: 'main.html',
       port: '3000',
       open: true
     }));
+});
+
+gulp.task('default', function () {
+  runSequence('copyMainHtml', 'browserify', 'webserver');
 });
