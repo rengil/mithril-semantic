6ab86fca5ca022a413810ef72e71131ec6734782
renan.gil <renan.zgil@gmail.com>
2016-07-14 13:19:25 -0300

    semantic ui

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,19 @@ const browserify = require('gulp-browserify');
 const runSequence = require('run-sequence');
 const msx = require('gulp-msx');
 const del = require('del');
+const symlink = require('gulp-sym');
+const less = require('gulp-less');
 
 gulp.task('js', function (){
   return gulp.src('public/js/**/*.js')
   .pipe(msx({harmony: true}))
   .pipe(gulp.dest('public/temp/'));
+});
+
+gulp.task('css', function (){
+  return gulp.src('public/css/**/*.less')
+  .pipe(less())
+  .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('browserify', function() {
@@ -37,6 +45,23 @@ gulp.task('webserver', function() {
     }));
 });
 
+gulp.task('semantic-themes', function () {
+  return gulp.src('./node_modules/semantic-ui-less/themes/**')
+  .pipe(gulp.dest('public/themes'));
+});
+
+
+gulp.task('remove-semantic-configurable', function () {
+  del(['node_modules/semantic-ui-less/theme.config', 'node_modules/semantic-ui-less/site']);
+});
+
+gulp.task('get-configurable-semantic', function () {
+  gulp.src(['public/css//theme.config', 'lib/css//site'])
+    .pipe(symlink(['node_modules/semantic-ui-less/theme.config',
+                   'node_modules/semantic-ui-less/site']));
+});
+
 gulp.task('default', function () {
   runSequence('copyMainHtml', 'js' , 'browserify', 'deleteTempFiles' , 'webserver');
+  runSequence('semantic-themes', 'remove-semantic-configurable' , 'get-configurable-semantic', 'css');
 });
